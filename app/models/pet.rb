65d53e298edd3e4ccc253bb969c219ae391c35aa
Enrique Ramirez <nrqrmz@gmail.com>
2025-05-17 16:50:59 -0600

    add crud actions and simple_form_for

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,13 @@
 class Pet < ApplicationRecord
+  VALID_SPECIES = %w[dog cat reptile bug horse].freeze
+
   validates :name, presence: true
   validates :address, presence: true
   validates :found_on, presence: true
-  validates :species, inclusion: { in: %w[dog cat reptile bug horse] }
+  validates :species, inclusion: { in: VALID_SPECIES }
+
+  def days_ago
+    days_difference = (Date.today - found_on).to_i
+    "Found #{days_difference} days ago"
+  end
 end
